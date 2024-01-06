63fa14c897f11e271fe45b6c7ab2a921dbe82bf2
VoidAlt <152700023+VoidAlt@users.noreply.github.com>
2024-01-06 16:46:38 +0530

    Update config.py
    Signed-off-by: VoidAlt <152700023+VoidAlt@users.noreply.github.com>

--- config.py
+++ config.py
@@ -38,8 +38,8 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
    # "GIT_TOKEN", None
   # Fill this variable if your upstream repository is private
 
-#SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-#SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VoidxSupport")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/VoidxUpdates")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
